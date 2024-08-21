bf60926e057e246f6dc63bf4bb2453c005fd3fa6
saviovnn <savioviannasilva@hotmail.com>
2024-08-21 16:35:57 -0300

    primeiro commit

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import CountriesTable from "../components/CountriesTable/CountriesTable";
+import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
+import styles from "../styles/Home.module.css";
+
+export default function Home({ countries }) {
+  console.log(countries);
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+
+      <SearchInput placeholder="Filter by Name Region or SubRegion" />
+
+      <CountriesTable countries={countries} />
+    </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
